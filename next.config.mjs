4871cedfaf0c364ac8b7a9226a28dc70e376c442
yogyy <yogimr261@gmail.com>
2023-05-26 07:34:16 +0700

    add typecheck, ignore build errors on vercel, swcMinify

--- next.config.mjs
+++ next.config.mjs
@@ -21,5 +21,13 @@ const config = {
     locales: ["en"],
     defaultLocale: "en",
   },
+
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  swcMinify: true,
 };
 export default config;
