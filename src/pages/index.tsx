765a37389f61ed6d82d2573679852edf2b614594
yogyy <yogimr261@gmail.com>
2023-08-17 21:26:55 +0700

    chore: delete useless code

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,34 +6,35 @@ import { LoadingPage, LoadingSpinner } from "~/components/loading";
 import { PageLayout } from "~/components/layout";
 import { PostView } from "~/components/postView";
 import { CreateWizzardPost } from "~/components/CreateWizzardPost";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "~/components/ui/tabs";
-import { useLoadingContext } from "~/context/context";
+import { LuTwitter } from "react-icons/lu";
 
 const Feed = () => {
   const { data, isLoading: postLoading } = api.posts.getAll.useQuery();
 
-  // console.log(context);
   return (
     <div className="h-auto w-full">
-      {postLoading ? (
+      {postLoading && (
         <div className="flex h-screen items-center justify-center">
           <LoadingSpinner size={60} />
         </div>
-      ) : !data ? (
-        <div>Something went wrong! </div>
-      ) : (
+      )}
+      {!data && <div>Something went wrong! </div>}
+      {data &&
         data.map((fullPost) => (
           <PostView {...fullPost} key={fullPost.post.id} />
-        ))
-      )}
+        ))}
     </div>
   );
 };
 
 const Home: NextPage = () => {
-  const { user, isLoaded: userLoaded, isSignedIn } = useUser();
-  // console.log(loading);
-  if (!userLoaded) return <div />;
+  const { isLoaded: userLoaded, isSignedIn } = useUser();
+  if (!userLoaded)
+    return (
+      <div className="flex h-screen w-screen items-center justify-center">
+        <LuTwitter size={100} className="fill-slate-300 stroke-slate-300" />
+      </div>
+    );
   return (
     <PageLayout className="">
       <div className="flex w-full flex-col border-x border-border md:w-[42rem]">
@@ -64,25 +65,3 @@ const Home: NextPage = () => {
 };
 
 export default Home;
-
-{
-  /* <Tabs defaultValue="account" className="h-[53px] w-full">
-        <div className="my-3 h-[53px] px-3 text-[20px] font-semibold">
-          <h1>Beranda</h1>
-        </div>
-          <TabsList>
-            <TabsTrigger value="account">For You</TabsTrigger>
-            <TabsTrigger value="password">Following</TabsTrigger>
-          </TabsList>
-          <div className="flex border-b border-border p-4">
-            {!isSignedIn && <SignInButton />}
-            {isSignedIn && <CreateWizzardPost />}
-          </div>
-          <TabsContent value="account" className="">
-            <Feed />
-          </TabsContent>
-          <TabsContent value="password">
-            <p>not following anyone</p>
-          </TabsContent>
-        </Tabs> */
-}
