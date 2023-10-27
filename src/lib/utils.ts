502db35735b1b3e58530f4f95db6b19df0b829ca
yogyy <yogimr261@gmail.com>
2023-10-27 23:33:22 +0700

    add new utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,32 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function convertToHttps(url: string):
+  | {
+      href: string;
+      title: string;
+    }
+  | undefined {
+  if (!url) return;
+
+  if (url.startsWith("https://")) {
+    return {
+      href: url,
+      title: url.slice(8),
+    };
+  } else if (url.startsWith("http://")) {
+    return {
+      href: "https://" + url.slice(7),
+      title: url.slice(7),
+    };
+  } else {
+    return {
+      href: "https://" + url,
+      title: url,
+    };
+  }
 }
