724fea2b9978a9945a7c25a44112dc05caede48a
yogyy <yogimr261@gmail.com>
2023-10-28 23:47:26 +0700

    feat: add correct date tweet created

--- src/components/tweet/tweet-title.tsx
+++ src/components/tweet/tweet-title.tsx
@@ -1,24 +1,44 @@
 import Link from "next/link";
 import React from "react";
-import { ToolTip } from "../tooltip";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "~/components/ui/alert-dialog";
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "~/components/ui/popover";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "~/components/ui/tooltip";
+
 import { BsThreeDots } from "react-icons/bs";
 import { api } from "~/utils/api";
 import dayjs from "dayjs";
 import type { UserResource } from "@clerk/types";
 import { toast } from "react-hot-toast";
-import { TweetType } from "./tweet-detail";
+import { TweetType } from "./tweet-post";
+import { BiSolidTrash } from "react-icons/bi";
+import { cn } from "~/lib/utils";
+import { tweetTime } from "~/lib/tweet";
 
 type TweetTitleType = TweetType & {
   user: UserResource | null | undefined;
 };
 
 const TweetTitle: React.FC<TweetTitleType> = (props) => {
-  const { author, post, user } = props;
+  const { author, post, user, variant } = props;
   const ctx = api.useUtils();
 
   const { mutate, isLoading: isDeleting } = api.posts.deleteById.useMutation({
@@ -40,57 +60,99 @@ const TweetTitle: React.FC<TweetTitleType> = (props) => {
     mutate({ id: post.id });
   };
 
+  const currentTime = dayjs();
+
   return (
-    <div className="-mt-0.5 flex h-5 w-full flex-wrap items-start">
-      <Link
-        onClick={(e) => e.stopPropagation()}
-        className="-mt-0.5 flex items-start break-words font-sans text-base font-bold outline-none focus-within:underline hover:underline"
-        href={`/@${author.username}`}
-      >
-        {`${author.firstName} ${
-          author.lastName !== null ? author.lastName : ""
-        }`}
-      </Link>
-      <Link
-        tabIndex={-1}
-        onClick={(e) => e.stopPropagation()}
-        className="ml-2 inline-flex text-accent outline-none"
-        href={`/@${author.username}`}
-      >
-        {`@${author.username}`}
-      </Link>
-      <span className="px-1">·</span>
-      <Link
-        href={`/post/${post.id}`}
-        className="group relative flex w-max items-end text-sm font-thin text-accent outline-none hover:underline focus:underline"
-        aria-label={dayjs(post.createdAt).format("LL LT")}
-      >
-        <time dateTime={post.createdAt.toISOString()}>{`${dayjs(
-          post.createdAt
-        ).format("DD MMM")}`}</time>
-        <ToolTip tip={dayjs(post.createdAt).format("LT LL")} />
-      </Link>
+    <div className="flex w-full flex-wrap items-start justify-between">
+      <div className={cn("flex", variant === "details" && "flex-col")}>
+        <Link
+          onClick={(e) => e.stopPropagation()}
+          className="-mt-0.5 flex items-start break-words font-sans text-base font-bold outline-none focus-within:underline hover:underline"
+          href={`/@${author.username}`}
+        >
+          {`${author.firstName} ${
+            author.lastName !== null ? author.lastName : ""
+          }`}
+        </Link>
+        <Link
+          tabIndex={-1}
+          onClick={(e) => e.stopPropagation()}
+          className="ml-2 inline-flex text-accent outline-none"
+          href={`/@${author.username}`}
+        >
+          {`@${author.username}`}
+        </Link>
+        <span className="px-1">·</span>
+        <Link
+          href={`/post/${post.id}`}
+          className="group relative flex w-max items-end text-sm font-thin text-accent outline-none hover:underline focus:underline"
+          aria-label={dayjs(post.createdAt).format("LL LT")}
+        >
+          <TooltipProvider>
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <time dateTime={post.createdAt.toISOString()}>
+                  {tweetTime(post.createdAt)}
+                </time>
+              </TooltipTrigger>
+              <TooltipContent
+                side="bottom"
+                className="rounded-none border-none bg-[#495A69] p-1 font-sans text-xs text-white"
+              >
+                {dayjs(post.createdAt).format("LT LL")}
+              </TooltipContent>
+            </Tooltip>
+          </TooltipProvider>
+        </Link>
+      </div>
 
       {user?.id === author.id ? (
-        <Popover>
-          <PopoverTrigger
-            onClick={(e) => e.stopPropagation()}
-            className="ml-auto inline-flex h-7 w-7 items-center justify-center rounded-full p-1 hover:bg-primary/25"
-          >
-            <BsThreeDots className="text-base" />
-          </PopoverTrigger>
-          <PopoverContent>
-            <button
-              onClick={
-                user?.id === author.id
-                  ? deletePost
-                  : () => console.log("bukan author")
-              }
+        <div className="">
+          <Popover>
+            <PopoverTrigger
+              onClick={(e) => e.stopPropagation()}
+              className="inline-flex h-7 w-7 items-center justify-center rounded-full p-1 hover:bg-primary/25"
             >
-              delete
-            </button>
-          </PopoverContent>
-        </Popover>
+              <BsThreeDots className="text-base" />
+            </PopoverTrigger>
+            <PopoverContent
+              onClick={(e) => e.stopPropagation()}
+              side="left"
+              sideOffset={-30}
+              align="start"
+            >
+              <AlertDialog>
+                <AlertDialogTrigger onClick={(e) => e.stopPropagation()}>
+                  <BiSolidTrash /> Hapus
+                </AlertDialogTrigger>
+                <AlertDialogContent>
+                  <AlertDialogHeader>
+                    <AlertDialogTitle>Hapus postingan?</AlertDialogTitle>
+                    <AlertDialogDescription>
+                      Ini tidak dapat dibatalkan dan akan dihapus dari profil
+                      Anda, timeline akun yang mengikuti Anda, dan dari hasil
+                      pencarian.
+                    </AlertDialogDescription>
+                  </AlertDialogHeader>
+                  <AlertDialogFooter>
+                    <AlertDialogAction
+                      className="bg-red-600"
+                      onClick={
+                        user?.id === author.id
+                          ? deletePost
+                          : () => console.log("bukan author")
+                      }
+                    >
+                      Hapus
+                    </AlertDialogAction>
+                    <AlertDialogCancel>Batalkan</AlertDialogCancel>
+                  </AlertDialogFooter>
+                </AlertDialogContent>
+              </AlertDialog>
+              ;
+            </PopoverContent>
+          </Popover>
+        </div>
       ) : null}
     </div>
   );
--- src/lib/tweet.ts
+++ src/lib/tweet.ts
@@ -1,3 +1,5 @@
+import dayjs from "dayjs";
+
 export const URL_REGEX =
   /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/gm;
 
@@ -5,4 +7,27 @@ export const renderText = (text: string): string => {
   const textWithoutEmptyLines = text.replace(/^\s*$/gm, " ");
   const textWithSingleLineBreaks = textWithoutEmptyLines.replace(/\n+/g, "\n");
   return textWithSingleLineBreaks;
+};
+
+export const tweetTime = (date: Date) => {
+  const currentTime = dayjs();
+  const tweetDate = dayjs(date);
+
+  const secondsDiff = currentTime.diff(tweetDate, "second");
+
+  if (secondsDiff >= 60) {
+    const minutesDiff = currentTime.diff(tweetDate, "minute");
+
+    if (minutesDiff >= 60) {
+      const hoursDiff = currentTime.diff(tweetDate, "hour");
+      if (hoursDiff >= 24) {
+        return tweetDate.format("DD MMM");
+      }
+      return `${hoursDiff}h`;
+    }
+
+    return `${minutesDiff}m`;
+  }
+
+  return `${secondsDiff}s`;
 };
