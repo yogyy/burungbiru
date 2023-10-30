c5c0cdb7b8d983acb2ad36848e120635d0e47201
yogyy <yogimr261@gmail.com>
2023-10-31 00:00:00 +0700

    feat(navbar): add navbar icons

---
+++ src/components/icons/navbar-icon/index.ts
@@ -0,0 +1,25 @@
+import Bell from "./bell.svg";
+import Bookmark from "./bookmark.svg";
+import Group from "./group.svg";
+import Home from "./home.svg";
+import List from "./list.svg";
+import Logo from "./logo.svg";
+import Menu from "./menu.svg";
+import Message from "./message.svg";
+import Person from "./person.svg";
+import PersonFill from "./person-fill.svg";
+import Search from "./search.svg";
+
+export {
+  Bell,
+  Bookmark,
+  Group,
+  Home,
+  List,
+  Logo,
+  Menu,
+  Message,
+  Person,
+  Search,
+  PersonFill,
+};
