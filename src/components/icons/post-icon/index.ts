ecdaf79a256e3dc46bf95e1927c527432ed18850
yogyy <yogimr261@gmail.com>
2023-10-30 23:57:36 +0700

    feat(svg): add post icons

---
+++ src/components/icons/post-icon/index.ts
@@ -0,0 +1,8 @@
+import Share from "./share.svg";
+import Analytic from "./analytic.svg";
+import Bookmark from "./bookmark.svg";
+import Comment from "./comment.svg";
+import Like from "./Like.svg";
+import Retweet from "./retweet.svg";
+
+export { Analytic, Bookmark, Comment, Like, Retweet, Share };
