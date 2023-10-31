520acd4ffe6a6cc2eca1f536c342fd024c536545
yogyy <yogimr261@gmail.com>
2023-10-31 23:36:05 +0700

    chore: fix styling

--- src/components/tweet/tweet-action.tsx
+++ src/components/tweet/tweet-action.tsx
@@ -1,56 +1,61 @@
 import React from "react";
-import { Analytic, Bookmark, Comment, Like, Retweet, Share } from "../icons";
+import { TweetButton } from "~/constant";
 import { cn } from "~/lib/utils";
+import { Button } from "../ui/button";
 
-const TweetButton = [
-  { name: "Reply", icon: Comment, action: null },
-  { name: "Repost", icon: Retweet, action: null },
-  { name: "Like", icon: Like, action: null },
-  { name: "Analytic", icon: Analytic, action: null },
-  { name: "Bookmark", icon: Bookmark, action: null },
-  { name: "Share", icon: Share, action: null },
-];
-export const TweetAction = () => {
+export const TweetAction = (props: React.HTMLAttributes<HTMLDivElement>) => {
   return (
-    <div className="relative flex">
-      <div className="mt-3 flex h-5 flex-1 flex-row gap-1">
-        {TweetButton.map((button) => (
+    <div className="relative z-10 -ml-2 -mr-2 flex" {...props}>
+      <div className="mt-3 flex h-5 flex-1 flex-row gap-1 border-x border-transparent">
+        {TweetButton.map((btn) => (
           <div
             className={cn(
-              "flex w-full flex-1 cursor-pointer text-accent",
-              button.name === "Share" && "w-fit flex-none justify-end",
-              button.name === "Bookmark" && "w-fit flex-none justify-end"
+              "flex w-full flex-1 text-accent",
+              btn.name === "Share" && "w-fit flex-none justify-end",
+              btn.name === "Bookmark" && "w-fit flex-none justify-end"
             )}
-            key={button.name}
+            key={btn.name}
           >
-            <div className="group flex items-center">
-              <button
-                onClick={(e) => {
-                  e.stopPropagation();
-                  button.action;
-                }}
+            <div
+              className="group flex items-center"
+              onClick={(e) => e.stopPropagation()}
+            >
+              <Button
+                size="icon"
+                variant="ghost"
+                onClick={() => btn.action}
+                type="button"
                 className={cn(
-                  "-mr-2 inline-flex h-9 w-9 items-center justify-center rounded-full transition duration-300 last:mr-0 hover:bg-primary/20",
-                  button.name === "Repost" && "hover:bg-[#00BA7C]/20",
-                  button.name === "Like" && "hover:bg-[#F91880]/20"
+                  "group/button z-10 -mr-2 flex border-2 transition-all ease-in",
+                  "last:mr-0 hover:bg-primary/10 focus-visible:border-primary/50",
+                  btn.name === "Repost" &&
+                    "hover:bg-[#00BA7C]/10 focus-visible:border-[#00BA7C]/50",
+                  btn.name === "Like" &&
+                    "hover:bg-[#F91880]/10 focus-visible:border-[#F91880]/50",
+                  ""
                 )}
               >
                 <span
                   className={cn(
-                    "w-5 fill-accent transition duration-300 group-hover:fill-primary",
-                    button.name === "Repost" && "group-hover:fill-[#00BA7C]",
-                    button.name === "Like" && "group-hover:fill-[#F91880]"
+                    "w-5 fill-accent transition duration-300 group-hover:fill-primary group-focus-visible/button:fill-primary",
+                    btn.name === "Repost" &&
+                      "group-hover:fill-[#00BA7C] group-focus-visible/button:fill-[#00BA7C]",
+                    btn.name === "Like" &&
+                      "group-hover:fill-[#F91880] group-focus-visible/button:fill-[#F91880]"
                   )}
                 >
-                  <button.icon />
+                  <btn.icon />
                 </span>
-              </button>
-              {button.name !== "Bookmark" && button.name !== "Share" && (
+              </Button>
+              {btn.name !== "Bookmark" && btn.name !== "Share" && (
                 <span
                   className={cn(
-                    "h-fit min-w-[calc(1em_+_24px)] px-2 font-sans text-[13px] leading-4 transition duration-300 group-hover:text-primary",
-                    button.name === "Repost" && "group-hover:text-[#00BA7C]",
-                    button.name === "Like" && "group-hover:text-[#F91880]"
+                    "h-fit min-w-[calc(1em_+_24px)] px-2 font-sans text-[13px] leading-4 md:cursor-pointer",
+                    "transition duration-300 group-focus-within:text-primary group-hover:text-primary",
+                    btn.name === "Repost" &&
+                      "group-focus-within:text-[#00BA7C] group-hover:text-[#00BA7C]",
+                    btn.name === "Like" &&
+                      "group-focus-within:text-[#F91880] group-hover:text-[#F91880]"
                   )}
                 >
                   20
@@ -60,13 +65,6 @@ export const TweetAction = () => {
           </div>
         ))}
       </div>
-      {/* <div className="-mb-4 flex h-8 w-8 items-center justify-center">
-        <button disabled className="inline-flex h-5 w-5">
-          <span className="fill-accent">
-            <Share />
-          </span>
-        </button>
-      </div> */}
     </div>
   );
 };
