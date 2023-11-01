85bc7ac49c8b3c531dfa78a930d17907de8ddfad
yogyy <yogimr261@gmail.com>
2023-11-02 00:03:38 +0700

    fix: remove possibly username null

--- src/server/helper/filterforClient.ts
+++ src/server/helper/filterforClient.ts
@@ -1,9 +1,6 @@
 import type { User } from "@clerk/nextjs/api";
 
 export const filterUserForClient = (user: User) => {
-  if (user.username === null) {
-    usernamed: user.firstName;
-  }
   return {
     id: user.id,
     username: user.username,
