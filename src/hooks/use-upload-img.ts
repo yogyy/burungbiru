14e10e21b4a639ca810b353cec9af9dfa53ee14f
yogyy <yogimr261@gmail.com>
2023-11-03 21:17:42 +0700

    feat: add upload image hooks

---
+++ src/hooks/use-upload-img.ts
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import toast from "react-hot-toast";
+
+export function useUploadImage() {
+  const [image, setImage] = useState<File | string>("");
+  const [ImagePrev, setImagePrev] = useState("");
+
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files ?? [];
+    if (!files) return;
+    const file = files[0];
+    const maxSizeInBytes = 2 * 1024 * 1024;
+    if (!file) return;
+
+    if (file.size > maxSizeInBytes)
+      return toast(`Maximum Size Image 2MB`, {
+        position: "top-center",
+        style: { backgroundColor: "hsl(var(--desctructive))" },
+      });
+    setImage(file);
+    setImagePrev(URL.createObjectURL(file));
+  };
+
+  return { image, ImagePrev, setImagePrev, handleImageChange };
+}
