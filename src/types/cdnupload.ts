411f74850e0134829664c7d3cd4980d0fe1675ee
yogyy <yogimr261@gmail.com>
2023-11-05 01:42:17 +0700

    feat: upload cloudinary return types

---
+++ src/types/cdnupload.ts
@@ -0,0 +1,33 @@
+import { z } from "zod";
+
+type FileUploadInfo = {
+  event: string;
+  info: {
+    access_mode: string;
+    asset_id: string;
+    batchId: string;
+    bytes: number;
+    created_at: string;
+    etag: string;
+    folder: string;
+    format: string;
+    height: number;
+    id: string;
+    original_filename: string;
+    path: string;
+    placeholder: boolean;
+    public_id: string;
+    resource_type: string;
+    secure_url: string;
+    signature: string;
+    tags: string[];
+    thumbnail_url: string;
+    type: string;
+    url: string;
+    version: number;
+    version_id: string;
+    width: number;
+  };
+};
+
+export type { FileUploadInfo };
