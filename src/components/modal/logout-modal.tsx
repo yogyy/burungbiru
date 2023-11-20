321884a09d2e40c5773edfa96178daf695011cce
yogyy <yogimr261@gmail.com>
2023-11-20 23:54:33 +0700

    feat: separate logout modal

---
+++ src/components/modal/logout-modal.tsx
@@ -0,0 +1,76 @@
+import React from "react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../ui/dialog";
+import { Button } from "../ui/button";
+import { BsTwitterX } from "react-icons/bs";
+import { useClerk } from "@clerk/nextjs";
+import { UserResource } from "@clerk/types/dist";
+import { useBurgerMenu } from "~/hooks/store";
+
+export const LogoutModal: React.FC<{
+  user: UserResource | null | undefined;
+  children: React.ReactNode;
+}> = ({ user, children }) => {
+  const [modalSignOut, setModalSignOut] = React.useState(false);
+  const closeBurgerMenu = useBurgerMenu((state) => state.setShow);
+  const { signOut } = useClerk();
+
+  return (
+    <Dialog open={modalSignOut} onOpenChange={setModalSignOut}>
+      <DialogTrigger
+        asChild
+        onClick={(e) => {
+          e.stopPropagation();
+        }}
+      >
+        {children}
+      </DialogTrigger>
+      <DialogContent
+        close={false}
+        className="!rounded-2xl border-none p-8 text-start [&>button]:invisible"
+        overlayClassName="bg-background/90"
+      >
+        <DialogHeader className="space-y-0">
+          <BsTwitterX className="mb-4 h-9 w-full text-3xl" />
+          <DialogTitle className="!mb-2 text-xl font-semibold tracking-normal">
+            Log out of @{user?.username}
+          </DialogTitle>
+          <DialogDescription asChild>
+            <div className="flex flex-col gap-6">
+              <p className="break-words font-twitter-chirp text-[15px] text-accent">
+                This will only apply to this account, and you’ll still be logged
+                in to your other accounts.
+              </p>
+              <div className="flex flex-col text-[17px] font-bold">
+                <Button
+                  variant="secondary"
+                  onClick={() => {
+                    signOut();
+                    setModalSignOut((prev) => !prev);
+                    closeBurgerMenu((prev) => !prev);
+                  }}
+                  className="mb-3 min-h-[44px] min-w-[44px] text-[15px]"
+                >
+                  Log out
+                </Button>
+                <Button
+                  variant="outline"
+                  className="min-h-[44px] min-w-[44px] border-border text-[15px]"
+                  onClick={() => setModalSignOut((prev) => !prev)}
+                >
+                  Cancel
+                </Button>
+              </div>
+            </div>
+          </DialogDescription>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
+};
--- src/components/nav-logout.tsx
+++ src/components/nav-logout.tsx
@@ -5,22 +5,12 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from "./ui/popover";
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from "./ui/dialog";
-
 import { UserResource } from "@clerk/types/dist";
 import Image from "next/image";
 import { TbDots } from "react-icons/tb";
 import { Button } from "./ui/button";
-import { BsTwitterX } from "react-icons/bs";
-import { useClerk } from "@clerk/nextjs";
 import { cn } from "~/lib/utils";
+import { LogoutModal } from "./modal/logout-modal";
 
 type NavbarLogoutType = React.HTMLAttributes<HTMLDivElement> & {
   user: UserResource | null | undefined;
@@ -31,9 +21,7 @@ export const NavbarLogout: React.FC<NavbarLogoutType> = ({
   className,
   ...props
 }) => {
-  const [modalSignOut, setModalSignOut] = React.useState(false);
   const [popoverSignOut, setPopoverSignOut] = React.useState(false);
-  const { signOut } = useClerk();
 
   return (
     <div
@@ -82,61 +70,16 @@ export const NavbarLogout: React.FC<NavbarLogoutType> = ({
           alignOffset={-20}
           className="data-[side=bottom] sm:data-[align=center] relative px-0 py-3.5 font-sans text-[15px] leading-5 text-foreground/90"
         >
-          <Dialog open={modalSignOut} onOpenChange={setModalSignOut}>
-            <DialogTrigger
-              asChild
-              onClick={(e) => {
-                e.stopPropagation();
-              }}
+          <LogoutModal user={user}>
+            <Button
+              variant="ghost"
+              className={cn(
+                "h-full w-full justify-normal rounded-md hover:bg-[rgb(22,24,28)]"
+              )}
             >
-              <Button
-                variant="ghost"
-                className="h-full w-full justify-normal rounded-md hover:bg-[rgb(22,24,28)]"
-              >
-                Log Out @{user?.username}
-              </Button>
-            </DialogTrigger>
-            <DialogContent
-              close={false}
-              className="!rounded-2xl border-none p-8 text-start [&>button]:invisible"
-              overlayClassName="bg-background/90"
-            >
-              <DialogHeader className="space-y-0">
-                <BsTwitterX className="mb-4 h-9 w-full text-3xl" />
-                <DialogTitle className="!mb-2 text-xl font-semibold tracking-normal">
-                  Log out of @{user?.username}
-                </DialogTitle>
-                <DialogDescription asChild>
-                  <div className="flex flex-col gap-6">
-                    <p className="break-words font-twitter-chirp text-[15px] text-accent">
-                      This will only apply to this account, and you’ll still be
-                      logged in to your other accounts.
-                    </p>
-                    <div className="flex flex-col text-[17px] font-bold">
-                      <Button
-                        variant="secondary"
-                        onClick={() => {
-                          signOut();
-                          setModalSignOut((prev) => !prev);
-                          setPopoverSignOut((prev) => !prev);
-                        }}
-                        className="mb-3 min-h-[44px] min-w-[44px] text-[15px]"
-                      >
-                        Log out
-                      </Button>
-                      <Button
-                        variant="outline"
-                        className="min-h-[44px] min-w-[44px] border-border text-[15px]"
-                        onClick={() => setModalSignOut((prev) => !prev)}
-                      >
-                        Cancel
-                      </Button>
-                    </div>
-                  </div>
-                </DialogDescription>
-              </DialogHeader>
-            </DialogContent>
-          </Dialog>
+              Log Out @{user?.username}
+            </Button>
+          </LogoutModal>
           <PopoverArrow className="h-2 stroke-neutral-100" />
         </PopoverContent>
       </Popover>
