df91af1982bb64761249db4f52fa2dd5af393ae4
yogyy <yogimr261@gmail.com>
2023-11-22 23:15:05 +0700

    fix: remove svgr and add svg file
    add tsx icons component

--- src/components/icons/index.ts
+++ src/components/icons/index.ts
@@ -1,12 +1,1 @@
-// @ts-nocheck
-
-import Media from "./image.svg";
-import Gif from "./gif.svg";
-import Emoji from "./emoji.svg";
-import Location from "./location.svg";
-import Poll from "./poll.svg";
-import Schedule from "./schedule.svg";
-import Tweet from "./tweet.svg";
-import Globe from "./globe.svg";
-
-export { Media, Gif, Emoji, Location, Poll, Schedule, Tweet, Globe };
+export * from './twitter-icons'
