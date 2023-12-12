6a039cb85754548795bd8cac66a756c1dd79c0db
yogyy <yogimr261@gmail.com>
2023-12-12 23:48:18 +0700

    feat(webhook): add webhook for clerk register user

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,7 @@
 import { authMiddleware } from "@clerk/nextjs/server";
 
 export default authMiddleware({
+  publicRoutes: ["/api/webhook(.*)"],
   ignoredRoutes: [
     "/favicon.ico",
     "/fonts/chirp-medium-web.woff",
---
+++ src/pages/api/webhook.ts
@@ -0,0 +1,76 @@
+import { Webhook } from "svix";
+import { WebhookEvent } from "@clerk/nextjs/server";
+import { NextApiRequest, NextApiResponse } from "next";
+import { buffer } from "micro";
+import { prisma } from "~/server/db";
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "POST") {
+    return res.status(405);
+  }
+  // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
+  const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
+
+  if (!WEBHOOK_SECRET) {
+    throw new Error(
+      "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
+    );
+  }
+
+  // Get the headers
+  const svix_id = req.headers["svix-id"] as string;
+  const svix_timestamp = req.headers["svix-timestamp"] as string;
+  const svix_signature = req.headers["svix-signature"] as string;
+
+  // If there are no headers, error out
+  if (!svix_id || !svix_timestamp || !svix_signature) {
+    return res.status(400).json({ error: "Error occured -- no svix headers" });
+  }
+
+  console.log("headers", req.headers, svix_id, svix_signature, svix_timestamp);
+  // Get the body
+  const body = (await buffer(req)).toString();
+
+  // Create a new Svix instance with your secret.
+  const wh = new Webhook(WEBHOOK_SECRET);
+
+  let evt: WebhookEvent;
+
+  // Verify the payload with the headers
+  try {
+    evt = wh.verify(body, {
+      "svix-id": svix_id,
+      "svix-timestamp": svix_timestamp,
+      "svix-signature": svix_signature,
+    }) as WebhookEvent;
+  } catch (err) {
+    console.error("Error verifying webhook:", err);
+    return res.status(400).json({ Error: err });
+  }
+
+  const eventType = evt.type;
+
+  if (eventType === "user.created") {
+    const { id, ...attributes } = evt.data;
+
+    await prisma.user.create({
+      data: {
+        id: id,
+        imageUrl: attributes.image_url,
+        username: attributes.username as string,
+        name: `${attributes.first_name} ${attributes.last_name ?? ""}`,
+      },
+    });
+  }
+
+  return res.status(200).json({ response: "Success" });
+}
