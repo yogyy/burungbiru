3fbc0be502a14ba23be54369f89b7a42f2cf3323
yogyy <yogimr261@gmail.com>
2023-12-17 23:38:41 +0700

    fix: move function

--- src/components/button-follow.tsx
+++ src/components/button-follow.tsx
@@ -23,29 +23,41 @@ export const FollowButton: React.FC<FollowButtonProps> = ({
   const following = api.action.followUser.useMutation({
     onSuccess: () => {
       ctx.profile.userFollower.invalidate({ userId: user.id });
+      ctx.profile.getUserRandomUserDB.invalidate();
     },
   });
   const unfollow = api.action.unfollowUser.useMutation({
     onSuccess: () => {
       ctx.profile.userFollower.invalidate({ userId: user.id });
+      ctx.profile.getUserRandomUserDB.invalidate();
     },
   });
   const { user: currentUser } = useUser();
 
-  return !follower?.followers.some(
-    (follower) => follower.followingId === currentUser?.id
-  ) ? (
+  const userInFollowing = !follower?.followers.some(
+    (foll) => foll.followingId === currentUser?.id
+  );
+
+  function FollowAction(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    e.stopPropagation();
+    if (userInFollowing) {
+      following.mutate({ userId: user.id });
+    } else {
+      unfollow.mutate({ userId: user.id });
+    }
+  }
+
+  return userInFollowing ? (
     <Button
+      ref={hoverRef}
       variant="outline"
       type="button"
       className={cn(
-        "sticky border-2 border-transparent bg-white text-card hover:bg-white/80 focus-visible:border-primary",
+        "border border-transparent bg-white text-card hover:bg-white/80 focus-visible:border-primary",
         className
       )}
-      onClick={(e) => {
-        following.mutate({ userId: user.id });
-        e.stopPropagation();
-      }}
+      disabled={unfollow.isLoading}
+      onClick={FollowAction}
     >
       Follow
     </Button>
@@ -58,11 +70,8 @@ export const FollowButton: React.FC<FollowButtonProps> = ({
         "border border-border text-white hover:border-[rgb(244,33,46)] hover:bg-[rgb(244,33,46)]/[.15] hover:text-[rgb(244,33,46)] focus-visible:border-white",
         className
       )}
-      //   disabled
-      onClick={(e) => {
-        unfollow.mutate({ userId: user.id });
-        e.stopPropagation();
-      }}
+      disabled={following.isLoading}
+      onClick={FollowAction}
     >
       {isHover ? "Unfollow" : "Following"}
     </Button>
