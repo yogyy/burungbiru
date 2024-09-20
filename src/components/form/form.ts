12cbd1888c8b935ebc6847cfb56c2a3e8de07e62
yogyy <yogimr261@gmail.com>
2024-09-21 01:45:16 +0700

    chore: add reply schema

--- src/components/form/form.ts
+++ src/components/form/form.ts
@@ -2,16 +2,25 @@ import { z } from "zod";
 
 type CreateTweetVariant = "default" | "modal";
 
+const Image = z
+  .object({
+    public_id: z.string(),
+    secure_url: z.string(),
+  })
+  .optional();
+
 const tweetSchema = z.object({
   text: z
     .string()
     .min(1, { message: "tweet must contain at least 1 character(s)" }),
-  image: z
-    .object({
-      public_id: z.string(),
-      secure_url: z.string(),
-    })
-    .optional(),
+  image: Image,
 });
 
-export { tweetSchema, type CreateTweetVariant };
+const replySchema = z.object({
+  text: z
+    .string()
+    .min(1, { message: "reply must contain at least 1 character(s)" }),
+  image: Image,
+});
+
+export { tweetSchema, replySchema, type CreateTweetVariant };
