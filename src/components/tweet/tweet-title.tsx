ae0a752129ba65f53ccb4160d9f1265adcb71ba6
yogyy <yogimr261@gmail.com>
2024-09-20 23:07:18 +0700

    fix: disable keyboard focus in title when modal/dialog mode

--- src/components/tweet/tweet-title.tsx
+++ src/components/tweet/tweet-title.tsx
@@ -1,3 +1,4 @@
+import dayjs from "dayjs";
 import Link from "next/link";
 import {
   Tooltip,
@@ -5,13 +6,12 @@ import {
   TooltipProvider,
   TooltipTrigger,
 } from "~/components/ui/tooltip";
-import dayjs from "dayjs";
-import { TweetProps } from "./types";
 import { cn } from "~/lib/utils";
-import { tweetTime } from "~/lib/tweet";
-import { UserCard } from "../user-hover-card";
-import { Badge } from "../ui/badge";
 import { api } from "~/utils/api";
+import { tweetTime } from "~/lib/tweet";
+import { TweetProps } from "./types";
+import { Badge } from "../ui/badge";
+import { UserCard } from "../user-hover-card";
 
 interface TweetTitleProps
   extends React.HTMLAttributes<HTMLDivElement>,
@@ -73,7 +73,7 @@ export const TweetTitle = ({
           </>
         ) : (
           <>
-            <p className="-mt-0.5 flex w-full flex-shrink-0 items-end text-base font-bold outline-none">
+            <p className="-mt-0.5 flex text-base font-bold outline-none">
               {author.name}
               <Badge variant={author.type} />
             </p>
@@ -88,30 +88,38 @@ export const TweetTitle = ({
         {variant !== "details" && (
           <>
             <span className="px-1 text-[15px] leading-5 text-accent">·</span>
-            <Link
-              href={`/post/${post.id}`}
-              className="group relative flex w-max flex-shrink-0 items-end text-sm font-thin text-accent outline-none hover:underline focus:underline"
-              aria-label={dayjs(data?.createdAt).format("LL LT")}
-            >
-              <TooltipProvider>
-                <Tooltip>
-                  <TooltipTrigger
-                    asChild
-                    className="text-[15px] font-normal leading-5"
-                  >
-                    <time dateTime={data?.createdAt.toISOString()}>
-                      {tweetTime(data?.createdAt ?? post.createdAt)}
-                    </time>
-                  </TooltipTrigger>
-                  <TooltipContent
-                    side="bottom"
-                    className="rounded-none border-none bg-[#495A69] p-1 text-xs text-white"
-                  >
-                    {dayjs(data?.createdAt).format("LT LL")}
-                  </TooltipContent>
-                </Tooltip>
-              </TooltipProvider>
-            </Link>
+            {type === "default" ? (
+              <Link
+                href={`/post/${post.id}`}
+                className="group relative flex w-max flex-shrink-0 items-end text-sm font-thin text-accent outline-none hover:underline focus:underline"
+                aria-label={dayjs(data?.createdAt).format("LL LT")}
+              >
+                <TooltipProvider>
+                  <Tooltip>
+                    <TooltipTrigger
+                      asChild
+                      className="text-[15px] font-normal leading-5"
+                    >
+                      <time dateTime={data?.createdAt.toISOString()}>
+                        {tweetTime(data?.createdAt ?? post.createdAt)}
+                      </time>
+                    </TooltipTrigger>
+                    <TooltipContent
+                      side="bottom"
+                      className="rounded-none border-none bg-[#495A69] p-1 text-xs text-white"
+                    >
+                      {dayjs(data?.createdAt).format("LT LL")}
+                    </TooltipContent>
+                  </Tooltip>
+                </TooltipProvider>
+              </Link>
+            ) : (
+              <p className="text-[15px] font-normal leading-5 text-accent">
+                <time dateTime={data?.createdAt.toISOString()}>
+                  {tweetTime(data?.createdAt ?? post.createdAt)}
+                </time>
+              </p>
+            )}
           </>
         )}
       </div>
