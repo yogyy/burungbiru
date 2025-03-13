f8a8767d43bfd85ebaa2e683a04cc81780df832d
yogyy <yogimr261@gmail.com>
2025-03-14 00:13:35 +0700

    chore: update props

--- src/components/tweet/actions/share-tweet.tsx
+++ src/components/tweet/actions/share-tweet.tsx
@@ -7,16 +7,17 @@ import { useCopyToClipboard } from "usehooks-ts";
 import { authClient } from "~/lib/auth-client";
 
 interface ShareTweetProps {
-  post: TweetProps;
   variant: VariantTweet;
+  postId: string;
+  author: TweetProps["author"];
 }
-export const ShareTweet = ({ variant, post }: ShareTweetProps) => {
+export const ShareTweet = ({ variant, author, postId }: ShareTweetProps) => {
   const [_, copy] = useCopyToClipboard();
   const { data } = authClient.useSession();
 
   function copyPostUrl() {
-    copy(`${window.location.origin}/p/${post.author.username}/${post.id}`);
-    toast.success("Copied to clipboard", { id: `copy ${post.id}` });
+    copy(`${window.location.origin}/post/${postId}`);
+    toast.success("Copied to clipboard", { id: `copy ${postId}` });
   }
 
   return (
@@ -26,11 +27,11 @@ export const ShareTweet = ({ variant, post }: ShareTweetProps) => {
           variant="ghost"
           type="button"
           size="icon"
-          disabled={data?.user.username === post.author.username}
+          disabled={data?.user.username === author?.username}
           onClick={copyPostUrl}
           className={cn(
             "group/button z-10 flex border-2 transition-all ease-in",
-            data?.user.username === post.author.username
+            data?.user.username === author?.username
               ? "hover:bg-none"
               : "hover:bg-primary/10 focus-visible:border-primary/50 focus-visible:bg-primary/10 group-hover:bg-primary/10"
           )}
@@ -39,7 +40,7 @@ export const ShareTweet = ({ variant, post }: ShareTweetProps) => {
             className={cn(
               variant === "details" ? "h-5 w-5" : "h-[18px] w-[18px]",
               "fill-accent transition duration-300",
-              data?.user.username !== post.author.username &&
+              data?.user.username !== author?.username &&
                 "group-hover:fill-primary group-focus-visible/button:fill-primary"
             )}
           />
