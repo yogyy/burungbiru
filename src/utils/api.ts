3572450d2343f792521569cb947ba17b5384a6ab
yogyy <yogimr261@gmail.com>
2025-03-18 22:32:26 +0700

    disable refetchOnWindowFocus on all query

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -21,6 +21,7 @@ const getBaseUrl = () => {
 export const api = createTRPCNext<AppRouter>({
   config() {
     return {
+      queryClientConfig: { defaultOptions: { queries: { refetchOnWindowFocus: false } } },
       /**
        * Transformer used for data de-serialization from the server.
        *
