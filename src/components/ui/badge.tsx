01e2f4dc5c7f0b985730137b8ceb58f91fdc66d4
yogyy <yogimr261@gmail.com>
2025-08-16 21:11:21 +0700

    fix: delete developer icon

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import { cn } from "~/lib/utils";
 import { User } from "@prisma/client";
 import { UserVerified } from "../icons/twitter-icons";
-import { DevIcon } from "../icons/developer-icon";
 
 export interface BadgeProps extends React.HTMLAttributes<HTMLSpanElement> {
   variant: User["type"];
@@ -12,8 +11,7 @@ function Badge({ className, variant, ...props }: BadgeProps) {
   if (variant === "user") return null;
   return (
     <span className={cn("ml-0.5", className)} {...props}>
-      {variant === "verified" && <UserVerified size={20} fill="rgba(29,155,240,1.00)" />}
-      {variant === "developer" && <DevIcon size={22} />}
+      <UserVerified size={20} fill="rgba(29,155,240,1.00)" />
     </span>
   );
 }
